247531d49776719f5e313341d4d5103a79017003
thiboschi <91882753+thiboschi@users.noreply.github.com>
2026-07-08 12:51:38 +0200

    initialisation du repo

---
+++ Parachev/src/Pages/Details_SubPage/Chiffrage.jsx
@@ -0,0 +1,12 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Chiffrage() {
+ 
+
+return (
+  <div className="page">
+    <h1>Chiffrage</h1>
+  </div>
+);
+}
---
+++ Parachev/src/Pages/Details_SubPage/Previsions.jsx
@@ -0,0 +1,12 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Previsions() {
+ 
+
+return (
+  <div className="page">
+    <h1>Previsions</h1>
+  </div>
+);
+}
