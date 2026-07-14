383eb9f72ad3a00e6bdd0cf9e476839bd77d2940
thiboschi <91882753+thiboschi@users.noreply.github.com>
2026-07-14 13:07:13 +0200

    add new shadcn component and add the react-router-dom

---
+++ Parachev/src/pages/DashBoard.tsx
@@ -0,0 +1,13 @@
+
+
+export default function DashBoard() {
+return (
+  <div>
+    <span>Hello There </span>
+    
+    {/* <nav>
+      <Link to="/affaire"> Test </Link>
+    </nav> */}
+  </div>
+);
+}
---
+++ Parachev/src/pages/Page404.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Page404() {
+return (
+  <div>
+    <span>Hello There </span>
+    
+    {/* <nav>
+      <Link to="/affaire"> Test </Link>
+    </nav> */}
+  </div>
+);
+}
