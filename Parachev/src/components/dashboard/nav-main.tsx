b813a83356f4f055efccf1466e8923d2ad064089
thiboschi <91882753+thiboschi@users.noreply.github.com>
2026-09-04 12:42:01 +0200

    update the sidebar button

--- Parachev/src/components/dashboard/nav-main.tsx
+++ Parachev/src/components/dashboard/nav-main.tsx
@@ -1,4 +1,4 @@
-import { IconCirclePlusFilled, type Icon } from "@tabler/icons-react"
+import { type Icon } from "@tabler/icons-react"
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import { NavLink } from "react-router-dom"
 
@@ -14,18 +14,6 @@ export function NavMain({
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
-              render={<NavLink to={"/create"} />}
-            >
-              <IconCirclePlusFilled />
-              <span>Create Affaire</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
